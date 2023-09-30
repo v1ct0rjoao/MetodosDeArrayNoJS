3ae83e92baf9f399efced9929d4ace3ffc4b4f65
victor <joao.victormeneses@ufrpe.br>
2023-09-30 15:30:02 -0300

    usando banco de dados e aplicando tecnicas de forEach e de array

---
+++ app/metodoForEach copy.js
@@ -0,0 +1,22 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+//ForEach (elemento, e o que ele deve fazer)
+// livros.forEach(livro => {
+//   console.log(livro)
+// })
+
+function exibirLivros(listaDelivros){
+    listaDelivros.forEach(livro => {
+      elementoParaInserirLivros.innerHTML += `
+      <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
+        <h2 class="livro__titulo">${livro.titulo}</h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>`
+      
+    })
+  }
+  
